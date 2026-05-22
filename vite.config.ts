2e80cf68f38200d81d7e7cc98f593183463be125
FrictionlessC0de <kalkidan.777el@gmail.com>
2026-05-22 03:33:44 +0300

    add cloudflare for deploying

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,28 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
-
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
 import viteReact, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
+import { cloudflare } from '@cloudflare/vite-plugin'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
+
   plugins: [
+    cloudflare({
+      viteEnvironment: {
+        name: 'ssr',
+      },
+    }),
+
     devtools(),
     tailwindcss(),
     tanstackStart(),
     viteReact(),
-    babel({ presets: [reactCompilerPreset()] }),
+    babel({
+      presets: [reactCompilerPreset()],
+    }),
   ],
 })
 
